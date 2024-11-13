eac4f8b7597d6cc1464346fa56b9661ab60f86f4
John Malc <cincenko@outlook.com>
2024-11-13 14:42:25 +0100

    add v3 suffix

--- internal/provider/const.go
+++ internal/provider/const.go
@@ -7,6 +7,7 @@ const (
 	api_groups    = "/org/groups"
 	api_auth      = "/authorize"
 	api_s3_suffix = "/s3-access-keys"
+	api_suffix = "/api/v3"
 	api_users     = "/org/users"
 	act           = "action"
 	n_act         = "not_action"
